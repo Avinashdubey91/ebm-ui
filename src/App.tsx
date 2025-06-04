766f088ac53e22d9f6e756b06a4adaa459f885af
Avinashdubey91 <45963285+Avinashdubey91@users.noreply.github.com>
2025-06-04 22:35:46 +0530

    Added env development and Production files and configuration related to them

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
+  useEffect(() => {
+    console.log("🌐 VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+  }, []);
+
   return (
     <div style={{ minHeight: '100vh', width: '100vw', overflow: 'hidden' }}>
       <Outlet />
